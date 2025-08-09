6e15f0347f8c9d4584adc60d69acbb80a149f2b2
SergioBS-pasoeco <sergiobolivars@gmail.com>
2025-08-09 12:09:24 +0200

    updated files

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of SDM_project is to ...
+The goal of SDM_project is to first of all learn how to sinchronized my local project with my repo in Github
 
--- git_connection.R
+++ git_connection.R
@@ -1,9 +1,7 @@
-
 library(gert)
 library(usethis)
 
 create_github_token()
-
 
 # 1) Ensure identity (skip if already set)
 usethis::use_git_config(user.name = "SergioBS-pasoeco", user.email = "sergiobolivars@gmail.com")
